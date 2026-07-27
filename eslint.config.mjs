33af2fb150800e8605d7c4198ef69a500995ce26
Guilherme <guilhermeoliveira6957@gmail.com>
2026-07-27 18:18:00 -0300

    Corrige ESLint definitivamente

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals.js";
-import nextTs from "eslint-config-next/typescript.js";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
 
-export default defineConfig([
-  ...nextVitals,
-  ...nextTs,
+export default defineConfig(
+  nextVitals,
+  nextTs,
   globalIgnores([
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
-  ]),
-]);
+  ])
+);
