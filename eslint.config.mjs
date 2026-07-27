7b4504798da4114dac4f52df3caee0a1df04d85f
Guilherme <guilhermeoliveira6957@gmail.com>
2026-07-27 18:11:27 -0300

    Corrige ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals.js";
-import nextTs from "eslint-config-next/typescript.js";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
 
 export default defineConfig([
-  ...nextVitals,
-  ...nextTs,
+  nextVitals,
+  nextTs,
   globalIgnores([
     ".next/**",
     "out/**",
